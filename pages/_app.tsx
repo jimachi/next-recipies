7b5e1768c0bb1a880c13f67ea6dbb94357084bc3
Yuichiro Iijima <jimaichiro@gmail.com>
2023-01-15 01:50:42 +0900

    add: MUIを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/private-theming";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "../theme";
+import createEmotionCache from "../createEmotionCache";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSizeEmotionCache = createEmotionCache();
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSizeEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+export default MyApp;
---
+++ theme.ts
@@ -0,0 +1,6 @@
+import { createTheme } from "@mui/material/styles";
+
+// TODO: 必要に応じてテーマを追加する
+const theme = createTheme({});
+
+export default theme;
